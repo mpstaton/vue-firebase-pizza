f1473b38c61f072872089dc92199547cd7d25141
Michael Staton <mpstaton@gmail.com>
2019-01-01 14:38:51 -0800

    added an Admin vue but it's not working for now

---
+++ src/components/Admin.vue
@@ -0,0 +1,33 @@
+<template>
+    <h1>Admin</h1>
+    <div class="row">
+        <div class="col-sm-12 col-md-6">
+         <!-- new pizza component -->
+        </div>
+        <div class="col-sm-12 col-md-6">
+            <h3>Menu:</h3>
+            <table class="table table-hover">
+               <thead class="thead-default">
+                   <tr>
+                       <th>Item</th>
+                       <th>Remove from menu></th>
+                   </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Margherita</td>
+                        <td><button class="btn btn-outline-danger btn-sm">x</button></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    
+}
+</script>
+
