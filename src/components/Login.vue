f5905d89648df02cf50f0bc636ebae6da2a5911a
Michael Staton <mpstaton@gmail.com>
2019-01-01 19:03:29 -0800

    connected login frontend to Firebase

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -11,10 +11,41 @@
                 <div class="form-group">
                     <input type="password" class="form-control" id="password" placeholder="Enter password">
                 </div>
-                <button type="button" class="btn btn-primary">Sign in</button>
-                <button type="button" class="btn btn-danger">Sign out</button>
+                <button type="button" class="btn btn-primary" @click.prevent="signIn">Sign in</button>
+                <button type="button" class="btn btn-danger" @click.prevent="signOut">Sign out</button>
             </div>
         </form>
     </div>
 
 </template>
+
+<script>
+import Firebase from 'firebase'
+
+export default {
+    methods: {
+        signIn() {
+            var email = document.getElementById('email').value;
+            var password = document.getElementById('password').value;
+
+            Firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+
+                if(errorCode === 'auth/wrong-password') {
+                    alert('Wrong password')
+                } else {
+                    alert(errorMessage);
+                }
+            }); 
+        },
+        signOut() {
+            Firebase.auth().signOut().then(function() {
+                alert('logged out')
+            }).catch(function(error) {
+                alert('error.')
+            });
+        }
+    }
+}
+</script>
