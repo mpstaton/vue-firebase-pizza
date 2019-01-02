03e3626ba40db714cf2a669dbe6dd2c2cb3451f2
Michael Staton <mpstaton@gmail.com>
2019-01-01 19:32:13 -0800

    Lecture 24, created Bootstrap Card components for Contact, About, Delivery, Ordering Guide

---
+++ src/components/Delivery.vue
@@ -0,0 +1,14 @@
+<template>
+        <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">Delivery Info</h5>
+            <p class="card-text">
+                <address>
+                    <strong>Pizza Planet</strong><br>
+                    Address Line <br>
+                    
+                </address>    
+            </p>
+        </div>
+        </div>
+</template>
---
+++ src/components/OrderingGuide.vue
@@ -0,0 +1,14 @@
+<template>
+        <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">How to order</h5>
+            <p class="card-text">
+                <address>
+                    <strong>Pizza Planet</strong><br>
+                    Address Line <br>
+
+                </address>    
+            </p>
+        </div>
+        </div>
+</template>
