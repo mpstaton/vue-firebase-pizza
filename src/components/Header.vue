b69a8497b2de3b558000796b5fcc43b24d4ebec5
Michael Staton <mpstaton@gmail.com>
2019-01-02 15:05:31 -0800

    added router tags and removed html tags

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,12 +3,8 @@
         <nav class="navbar navbar-toggleable-md navbar-light">
             <a class="navbar-brand" href="/">PIZZA PLANET</a>
             <ul class="navbar-nav">
-                <li class="nav-item">
-                    <router-link to="/"><a class="nav-link">home</a></router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link to="/menu"><a class="nav-link">menu</a></router-link>
-                </li>
+                    <router-link to="/" tag="li"><a class="nav-link">home</a></router-link>
+                    <router-link to="/menu" tag="li"><a class="nav-link">menu</a></router-link>
             </ul>    
         </nav>
     </header>
