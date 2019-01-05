d542f208b14fbe8ec94f29149679b0c37d06e537
Michael Staton <mpstaton@gmail.com>
2019-01-05 12:47:23 -0800

    implemeneted more component routes in main.js

--- src/main.js
+++ src/main.js
@@ -2,13 +2,25 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from './components/Home.vue'
 import Menu from './components/Menu.vue'
-import App from './App.vue'
+import About from './components/About.vue'
+import Contact from './components/Contact.vue'
+import Delivery from './components/Delivery.vue'
+import History from './components/History.vue'
+import OrderingGuide from './components/OrderingGuide.vue'
+import App from './App.vue';
+import { isAbsolute } from 'path';
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', component: Home },
   { path: '/menu/:items', component: Menu },
+  { path: '/about', components: About, children: [
+    { path: 'contact', components: Contact },
+    { path: 'history', components: History },
+    { path: 'contact', components: Contact },
+    { path: 'ordering-guide', components: OrderingGuide }
+  ]},
   { path: '*', redirect: '/'}
 ]
 
