c7b2359657d0205ff609606791a25c5e2b60464a
Michael Staton <mpstaton@gmail.com>
2019-01-05 13:15:50 -0800

    Lecture 34, nesting a router view, but it's not working in practice

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,3 +1,18 @@
 <template>
-    <h1>About Us:</h1>
+    <div>
+    <p>Click on the links below for more info.</p>
+        <nav class="navbar navbar-toggleable-md navbar-light">
+            <a class="navbar-brand" href="/">PIZZA PLANET</a>
+            <div>
+            <ul class="navbar-nav">
+                <router-link to="/history" tag="li"><a class="nav-link">our history</a></router-link>
+                <router-link to="/contact" tag="li"><a class="nav-link">contact us</a></router-link>
+                <router-link to="/ordering-guide" tag="li"><a class="nav-link">ordering guide</a></router-link>
+                <router-link to="/delivery" tag="li"><a class="nav-link">delivery</a></router-link>
+            </ul>
+            </div>    
+        </nav>
+        <router-view></router-view>
+    </div>
 </template>
+
