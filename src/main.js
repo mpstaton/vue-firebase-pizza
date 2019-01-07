c6d94fd7b8f0894e9849c083e3ed5394abc2b574
Michael Staton <mpstaton@gmail.com>
2019-01-06 16:04:39 -0800

    Lecture 41, Refactoring routes into a new src/routes.js file.

--- src/main.js
+++ src/main.js
@@ -1,33 +1,10 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from './components/Home.vue'
-import Menu from './components/Menu.vue'
-import About from './components/About.vue'
-import Admin from './components/Admin.vue'
-import Contact from './components/Contact.vue'
-import Delivery from './components/Delivery.vue'
-import History from './components/History.vue'
-import OrderingGuide from './components/OrderingGuide.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import { routes } from './routes';
 import App from './App.vue';
 import { isAbsolute } from 'path';
 
 Vue.use(VueRouter)
-
-const routes = [
-  { path: '/', name: 'homeLink', component: Home },
-  { path: '/menu/', name: 'menuLink', component: Menu },
-  { path: '/admin', name: 'adminLink', component: Admin, beforeEnter: (to, from, next) => {
-    alert('This area is for authorised users only, please login');
-    next();
-  } },
-  { path: '/about', name: 'aboutLink', component: About, children: [
-    { path: 'contact', name: 'contactLink', component: Contact },
-    { path: 'history', name: 'historyLink', component: History },
-    { path: 'delivery', name: 'deliveryLink', component: Delivery },
-    { path: 'ordering-guide', name: 'orderingGuideLink', component: OrderingGuide }
-  ]},
-  { path: '*', redirect: '/'}
-]
 
 //history is a feature of Vue that manages urls
 
---
+++ src/routes.js
@@ -0,0 +1,24 @@
+import Home from './components/Home.vue'
+import Menu from './components/Menu.vue'
+import About from './components/About.vue'
+import Admin from './components/Admin.vue'
+import Contact from './components/Contact.vue'
+import Delivery from './components/Delivery.vue'
+import History from './components/History.vue'
+import OrderingGuide from './components/OrderingGuide.vue'
+
+export const routes = [
+    { path: '/', name: 'homeLink', component: Home },
+    { path: '/menu/', name: 'menuLink', component: Menu },
+    { path: '/admin', name: 'adminLink', component: Admin, beforeEnter: (to, from, next) => {
+      alert('This area is for authorised users only, please login');
+      next();
+    } },
+    { path: '/about', name: 'aboutLink', component: About, children: [
+      { path: 'contact', name: 'contactLink', component: Contact },
+      { path: 'history', name: 'historyLink', component: History },
+      { path: 'delivery', name: 'deliveryLink', component: Delivery },
+      { path: 'ordering-guide', name: 'orderingGuideLink', component: OrderingGuide }
+    ]},
+    { path: '*', redirect: '/'}
+  ]
