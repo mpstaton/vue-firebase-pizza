3c146033327fe35f1f4f2ae38aa8b35ef05d0f9e
Michael Staton <mpstaton@gmail.com>
2019-01-13 11:27:55 -0800

    got firebase to work for orders

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -64,6 +64,7 @@
 <script>
 
 import { mapGetters } from 'vuex'
+import { dbOrdersRef } from '../firebaseConfig'
 
 export default {
     data() {
@@ -96,7 +97,8 @@ export default {
             item.quantity--;
         },
         addNewOrder() {
-            this.$store.commit('addOrder', this.basket)
+            //this.$store.commit('addOrder', this.basket)
+            dbOrdersRef.push(this.basket)
             this.basket = []
             this.basketText = "Thank you,your order has been placed"
         }
